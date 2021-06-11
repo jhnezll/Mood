252e76703c0f5722e2be3c4f67e59048cb0c2f8a
john <jhezell@go.olemiss.edu>
2021-06-11 03:26:13 -0500

    LoginView improved

--- Mood/screens/LoginView.swift
+++ Mood/screens/LoginView.swift
@@ -14,27 +14,73 @@ struct LoginView: View {
     @ObservedObject var sessionStore = SessionStore()
     
     var body: some View {
-        NavigationView {
-            VStack{
-                TextField("Email", text: $email)
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
-                SecureField("Password", text: $password)
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
-                Button(action: {
-                    sessionStore.signIn(email: email, password: password)
-                }, label: {
+        VStack{
+            Image(systemName: "book.closed.fill")
+                .resizable()
+                .frame(width: 60, height: 70)
+            
+            VStack {
+                HStack{
+                    
                     Text("Login")
-                })
+                        .font(.title)
+                        .fontWeight(.bold)
+                    Spacer(minLength: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/)
+                    
+                }
+                .padding(.top, 40)
                 
-                Button(action: {
-                    sessionStore.signUp(email: email, password: password)
-                }, label: {
-                    Text("Register")
-                })
+                VStack{
+                    
+                    HStack(spacing: 15){
+                        Image(systemName: "envelope.fill")
+                        
+                        TextField("Email", text: $email)
+                    }
+                    .padding(.horizontal)
+                    .padding(.top, 40)
+                }
+                
+                VStack{
+                    
+                    HStack(spacing: 15){
+                        Image(systemName: "eye.slash.fill")
+                        
+                        SecureField("Password", text: $password)
+                    }
+                    .padding(.horizontal)
+                    .padding(.top, 30)
+                    
+                    HStack{
+                        Button(action: {
+                            sessionStore.signIn(email: email, password: password)
+                        }) {
+                            Text("Sign In")
+                                .font(.title2)
+                                .opacity(/*@START_MENU_TOKEN@*/0.8/*@END_MENU_TOKEN@*/)
+                        }
+                        
+                        Spacer(minLength: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/)
+                        
+                        Button(action: {
+                            sessionStore.signUp(email: email, password: password)
+                        }) {
+                            Text("Register")
+                                .font(.title2)
+                                .opacity(/*@START_MENU_TOKEN@*/0.8/*@END_MENU_TOKEN@*/)
+                        }
+                    }
+                    .padding(.horizontal)
+                    .padding(.top, 30)
+                }
             }
-            .navigationBarTitle("Mood")
+            .padding()
+            .padding(.bottom, 65)
+            .padding(.horizontal, 20)
+            }
         }
-    }
+        
+        
 }
 
 struct LoginView_Previews: PreviewProvider {
--- Mood/screens/MainView.swift
+++ Mood/screens/MainView.swift
@@ -8,11 +8,26 @@
 import SwiftUI
 
 struct MainView: View {
+    
+    @ObservedObject var sessionStore = SessionStore()
+    
     var body: some View {
         TabView(selection: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Selection@*/.constant(1)/*@END_MENU_TOKEN@*/) {
-            JournalTab()
-            AddTab()
-            TimelineTab()
+            Button(action: {
+                sessionStore.signOut()
+            }, label: {
+                Text("Sign Out")
+            })
+            .tabItem { Text("Journal"); Image(systemName: "book.closed.fill") }.tag(1)
+            
+            
+            
+            Text("Tab Content 2").tabItem { Text("Add");
+                Image(systemName: "plus.square.fill")
+            }.tag(2)
+            Text("Tab Content 3").tabItem { Text("Timeline");
+                Image(systemName: "pencil.and.outline")
+            }.tag(3)
         }
     }
 }
@@ -23,24 +38,3 @@ struct MainView_Previews: PreviewProvider {
     }
 }
 
-struct JournalTab: View {
-    var body: some View {
-        Text("Tab Content hi").tabItem { Text("Journal"); Image(systemName: "book.closed.fill") }.tag(1)
-    }
-}
-
-struct AddTab: View {
-    var body: some View {
-        Text("Tab Content 2").tabItem { Text("Add");
-            Image(systemName: "plus.square.fill")
-        }.tag(2)
-    }
-}
-
-struct TimelineTab: View {
-    var body: some View {
-        Text("Tab Content 3").tabItem { Text("Timeline");
-            Image(systemName: "pencil.and.outline")
-        }.tag(2)
-    }
-}
