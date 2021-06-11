ba63dbb6d9b266a28bc5e22fcb842b501ef84430
john <jhezell@go.olemiss.edu>
2021-06-10 22:36:03 -0500

    Initial Commit

---
+++ Mood/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Mood
+//
+//  Created by John on 6/10/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Mood/MoodApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoodApp.swift
+//  Mood
+//
+//  Created by John on 6/10/21.
+//
+
+import SwiftUI
+
+@main
+struct MoodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
